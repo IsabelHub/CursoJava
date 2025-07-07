ca188e1e72e38e717f2b9a67a2dc386487de2b20
Tardes <IsabelHub>
2025-07-07 20:45:54 +0200

    segundo print

--- src/es/cursojava/inicio/holaMundo/HolaMundo.java
+++ src/es/cursojava/inicio/holaMundo/HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
-		System.out.println("Prueba para git");
+		System.out.println("Prueba para git 2.0");
 	}
 
 }
