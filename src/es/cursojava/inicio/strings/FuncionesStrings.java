13593c9b7a2d871fe5409dec346710556731f5f3
IsabelHub <Tardes@host.docker.internal>
2025-07-09 17:03:01 +0200

    commit invertido

---
+++ src/es/cursojava/inicio/strings/FuncionesStrings.java
@@ -0,0 +1,15 @@
+package es.cursojava.inicio.strings;
+
+public class FuncionesStrings {
+
+	public static void main(String[] args) {
+		String nombre = "Super man";
+		String nombre2 = "Eva";
+		nombre=nombre2;
+		System.out.println(nombre.toUpperCase());
+	
+		
+		
+	}
+
+}
---
+++ src/es/cursojava/inicio/strings/Invertido.java
@@ -0,0 +1,18 @@
+package es.cursojava.inicio.strings;
+
+public class Invertido {
+	public static void main(String[] args) {
+		String original = "Hola Mundo";
+		
+		String invertido = "";
+		
+        for (int i = original.length()- 1; i >= 0; i--) {
+            invertido += original.charAt(i);
+        }
+
+        System.out.println("Original: " + original);
+        System.out.println("Invertido: " + invertido);
+		
+		
+	}
+}
