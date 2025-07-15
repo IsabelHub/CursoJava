6d679e2490b38959d22a7777dacaeefc8621f162
Isabel Eclipse <Tardes@GET-09>
2025-07-15 20:49:45 +0200

    commit final BidimensionalesClase

--- src/es/cursojava/inicio/arrays/bidimensionales/BidimensionalesClase.java
+++ src/es/cursojava/inicio/arrays/bidimensionales/BidimensionalesClase.java
@@ -1,12 +1,24 @@
 package es.cursojava.inicio.arrays.bidimensionales;
-
 import java.util.Scanner;
+
+/*
+ * Ejercicio Colegio-Alumnos
+==============================================================
+Colegio con 2 aulas y 3 puestos en cada aula
+
+1. Insertar Alumnos -> Va a solicitar el nombre de los alumnos para las aulas que tengan puestos disponibles
+2. Mostrar Alumnos -> Muestra los alumnos por aula, si un puesto no tiene alumno, mostrar (vacio)
+3. Buscar Alumno -> Solicitar texto, Ju, mostrar aula y puesto en el que se encuentra el alumno que contenga el texto introducido
+4. Borrar Alumno -> Solcitar nombre, y se borra a todos los alumnos que tengan ese nombre.
+5. Salir
+ * */
+
 
 public class BidimensionalesClase {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int opcion;
-		String[][] aulas = new String[2][2]; // 2 aulas, 3 puestos cada una
+		String[][] aulas = new String[2][3]; // 2 aulas, 3 puestos cada una
 		do {
 			System.out.println("\n ------Elige una opción: -----");
 			System.out.println("1.Insertar Alumnos: ");
@@ -18,24 +30,33 @@ public class BidimensionalesClase {
 			opcion = sc.nextInt();
 			switch (opcion) {
 			case 1:
+				boolean hayHueco=false;
 				for (int i = 0; i < aulas.length; i++) {
 					for (int j = 0; j < aulas[i].length; j++) {
 						if (aulas[i][j] == null) {
 							System.out.print("Introduce nombre para aula " + (i + 1) + ", puesto " + (j + 1) + ": ");
 							sc = new Scanner(System.in);
 							String nombre = sc.nextLine();
-							aulas[i][j] = nombre;
+							aulas[i][j] = nombre;													
+							hayHueco=false;
 						}
 					}
 				}
+				if(!hayHueco) {
+					System.out.println("No hay puestos libres! ");
+				}
 				break;
 			case 2:
-				 for (String[] fila : aulas) {
-			            for (String alumno : fila) {
-			                System.out.print("\tAlumno:" + alumno + "\t");
-			            }
-			            System.out.println();
-			        }
+				for (String[] fila : aulas) {
+					for (String alumno : fila) {
+						if (alumno == null) {
+							System.out.println("No hay alumno asignado en ese puesto.");
+						} else {
+							System.out.print("\tAlumno:" + alumno + "\t");
+						}
+					}
+					System.out.println();
+				}
 				break;
 			case 3:
 				System.out.print("Introduce nombre a buscar: ");
@@ -44,16 +65,15 @@ public class BidimensionalesClase {
 				boolean encontrado = false;
 				for (int i = 0; i < aulas.length; i++) {
 					for (int j = 0; j < aulas[i].length; j++) {
-						if (aulas[i][j]==null) {
-							
+						//mirar codigo del profe, tiene un truqui para recorrer el 'if'
+						if (aulas[i][j]==null) {							
 							encontrado=false;												
 						}
 						else if(aulas[i][j].contains(cadenaBuscar)) {							
 								System.out.print("el alumno de nombre: " + cadenaBuscar + " esta en el aula: " + (i + 1) + ", puesto " + (j + 1) + ": ");
-								encontrado=true;
+					 			encontrado=true;
 						}
-					}
-					
+					}					
 				}
 				System.out.print("el alumno de nombre: " + cadenaBuscar + " no existe");
 				break;
@@ -64,10 +84,12 @@ public class BidimensionalesClase {
 				boolean escierto = false;
 				for (int i = 0; i < aulas.length; i++) {
 					for (int j = 0; j < aulas[i].length; j++) {
-						if (aulas[i][j].contains(cadenaBuscarDos)) {
+						if ( aulas[i][j]!=null && aulas[i][j].contains(cadenaBuscarDos)) {
 							aulas[i][j]=null;
 							escierto=true;							
 						}
+						else
+							aulas[i][j]=null;
 					}
 				}
 			
