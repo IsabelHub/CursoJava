2a4aa770d9b9aaf65422ac3c9472e138068fcea5
Isabel Eclipse <Tardes@GET-09>
2025-09-16 20:30:54 +0200

    commit holaMundo

--- src/es/cursojava/inicio/holaMundo/HolaMundo.java
+++ src/es/cursojava/inicio/holaMundo/HolaMundo.java
@@ -1,13 +1,19 @@
 package es.cursojava.inicio.holaMundo;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HolaMundo {
 
+	private static final Logger logger = LoggerFactory.getLogger(HolaMundo.class.getName());
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
 		System.out.println("Prueba para git 2.0");
+		logger.info("Hola 2");
+		logger.warn("Prueba");
 		
 	}
 
