6703cf6ac578109604e0ed2d14b816d250df1b19
Isabel Eclipse <Tardes@GET-09>
2025-09-25 17:34:16 +0200

    clase paciente

--- src/es/cursojava/herencia/hospital/Paciente.java
+++ src/es/cursojava/herencia/hospital/Paciente.java
@@ -1,5 +1,47 @@
 package es.cursojava.herencia.hospital;
 
-public class Paciente {
+import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import es.cursojava.herencia.bandamusica.Instrumento;
+
+public class Paciente extends Persona {
+	
+	private static final Logger logger = LoggerFactory.getLogger(Instrumento.class);
+
+	//no hace falta otorgarle dimension 
+	//porque inicializas el array sintomas directamente desde el constructor,
+	String[] sintomas;
+	
+	Persona persona = new Persona("Isabel", 100);
+
+	
+
+	public Paciente(String nombre, int edad, String[] sintomas) {
+		super(nombre, edad);
+		this.sintomas=sintomas;
+	}
+
+
+
+	public String[] getSintomas() {
+		return sintomas;
+	}
+
+
+	public boolean comer(){
+
+		if (comer())
+		{
+			logger.info("Paciente ... comiendo en la cafetería" + getNombre() );
+		}
+		return false;
+		
+	}
+
+	
 }
+
+
