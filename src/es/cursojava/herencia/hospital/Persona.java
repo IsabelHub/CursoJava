1ce5dcc78adf2ab13366c8fb5def0fd800b25a6d
Isabel Eclipse <Tardes@GET-09>
2025-09-25 16:58:56 +0200

    clase persona

--- src/es/cursojava/herencia/hospital/Persona.java
+++ src/es/cursojava/herencia/hospital/Persona.java
@@ -2,4 +2,26 @@ package es.cursojava.herencia.hospital;
 
 public class Persona {
 
+	private String nombre;
+	private int edad;
+	
+	public Persona(String nombre, int edad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+	
+	public static void comer() {
+		
+	}
+	
+	
 }
