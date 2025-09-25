770ed8ae7583759fdd51d1b4de165607feada699
Isabel Eclipse <Tardes@GET-09>
2025-09-25 17:34:36 +0200

    clase hospital

--- src/es/cursojava/herencia/hospital/Hospital.java
+++ src/es/cursojava/herencia/hospital/Hospital.java
@@ -1,10 +1,25 @@
 package es.cursojava.herencia.hospital;
 
+import es.cursojava.herencia.biblioteca.Libro;
+
 public class Hospital {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		String nombre;
+		int[] numHabitaciones = new int[2];
+		int[] numSalaDeEspera = new int[3];
+		
 
 	}
+	public static void abrirHospital() {
+		
+		//Paciente paciente = new Paciente();
+	}
+	/*
+	ficharEmpleados();
+	horaDeComer();
+	pasarConsultas();
 
+	 */
 }
