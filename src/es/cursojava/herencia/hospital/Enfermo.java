7d57f2a4caad771f7d2fe05f180bbe4c2a70c811
Isabel Eclipse <Tardes@GET-09>
2025-09-25 17:33:54 +0200

    clase enfermo

--- src/es/cursojava/herencia/hospital/Enfermo.java
+++ src/es/cursojava/herencia/hospital/Enfermo.java
@@ -1,5 +1,37 @@
 package es.cursojava.herencia.hospital;
 
-public class Enfermo {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import es.cursojava.herencia.bandamusica.Instrumento;
+
+public class Enfermo extends Persona{
+	
+	
+	private static final Logger logger = LoggerFactory.getLogger(Instrumento.class);
+
+	String enfermedad;
+
+	public Enfermo(String nombre, int edad,String enfermedad) {
+		super(nombre, edad);
+		this.enfermedad=enfermedad;
+	}
+
+	public String getEnfermedad() {
+		return enfermedad;
+	}
+
+	public boolean comer(){
+
+		if (comer())
+		{
+			logger.info(getNombre() + "Enfermo ... comiendo en la habitación"  );
+		}
+		return false;
+		
+	}
+
+	
+	
 
 }
