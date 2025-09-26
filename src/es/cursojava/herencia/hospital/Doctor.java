9f810ad748fc1dd7a8db9ce1bdbd0cddeca99932
Isabel Eclipse <Tardes@GET-09>
2025-09-26 17:07:58 +0200

    creacion clase Doctor

--- src/es/cursojava/herencia/hospital/Doctor.java
+++ src/es/cursojava/herencia/hospital/Doctor.java
@@ -1,5 +1,40 @@
 package es.cursojava.herencia.hospital;
 
-public class Doctor {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class Doctor extends EmpleadosHospital{
+	
+	
+	private static final Logger logger =   LoggerFactory.getLogger(Doctor.class);
+	String especialidad;
+
+	public Doctor(String nombre, int edad, String turno,String especialidad) {
+		super(nombre, edad, turno);
+		this.especialidad=especialidad;
+	}
+
+	public String getEspecialidad() {
+		return especialidad;
+	}
+
+	public boolean diagnosticarPaciente() {
+		
+
+		boolean estaEnfermo= false;
+		double numEnfermo = Math.random();
+		
+		if (numEnfermo>8)
+		{
+			estaEnfermo= true;
+		}
+		else
+		{
+			logger.info("El doctor "+ getNombre()+"valora que el paciente no esta enfermo" );
+		}
+		return estaEnfermo;
+	}
+	
+	
+	
 }
