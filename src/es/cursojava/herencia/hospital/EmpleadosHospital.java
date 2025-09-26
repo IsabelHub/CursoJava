7b3a7b59feba42d2a491df210703bd2b2204223f
Isabel Eclipse <Tardes@GET-09>
2025-09-26 17:36:47 +0200

    modificacion empleadosHospital

--- src/es/cursojava/herencia/hospital/EmpleadosHospital.java
+++ src/es/cursojava/herencia/hospital/EmpleadosHospital.java
@@ -1,5 +1,34 @@
 package es.cursojava.herencia.hospital;
 
-public class EmpleadosHospital {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import es.cursojava.herencia.hospital.EmpleadosHospital;
+
+public abstract class EmpleadosHospital extends Persona {
+
+	private static final Logger logger = LoggerFactory.getLogger(EmpleadosHospital.class);
+
+	String turno;
+
+	public EmpleadosHospital(String nombre, int edad, String turno) {
+		super(nombre, edad);
+		this.turno = turno;
+	}
+
+	public String getTurno() {
+		return turno;
+	}
+
+	public void fichar() {
+		logger.info( "El empleado " + getNombre() + "... está fichando");
+	}
+
+	@Override
+	public void comer() {
+		logger.info("Empleado ... comiendo en el comedor");
+
+	}
+
+	
 }
