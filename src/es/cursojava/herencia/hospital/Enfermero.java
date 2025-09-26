ad51d70610e6728b8f13a6c02d6f533d74dc4702
Isabel Eclipse <Tardes@GET-09>
2025-09-26 16:53:18 +0200

    creacion enfermero

--- src/es/cursojava/herencia/hospital/Enfermero.java
+++ src/es/cursojava/herencia/hospital/Enfermero.java
@@ -1,5 +1,22 @@
 package es.cursojava.herencia.hospital;
 
-public class Enfermero {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Enfermero extends EmpleadosHospital{
+
+	private static final Logger logger =   LoggerFactory.getLogger(Enfermero.class);
+	int planta ;
+	
+	
+	public Enfermero(String nombre, int edad, String turno) {
+		super(nombre, edad, turno);
+		
+	}
+	
+	
+	public void atenderPaciente() {
+		
+	}
 
 }
