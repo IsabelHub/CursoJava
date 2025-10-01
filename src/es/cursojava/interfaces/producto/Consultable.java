9bbc9bf229c6164816c231b6d4d340b5fc8b3767
Isabel Eclipse <Tardes@GET-09>
2025-10-01 16:29:53 +0200

    version 2 CRUD

---
+++ src/es/cursojava/interfaces/producto/Consultable.java
@@ -0,0 +1,6 @@
+package es.cursojava.interfaces.producto;
+
+public interface Consultable {
+
+	public  void select();//consultando por el id...
+}
---
+++ src/es/cursojava/interfaces/producto/Deletable.java
@@ -0,0 +1,10 @@
+package es.cursojava.interfaces.producto;
+
+public interface Deletable   {
+	
+	public void delete();//BOrrando ... con id
+
+	
+
+	
+}
