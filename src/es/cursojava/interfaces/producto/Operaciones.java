e1be4290de0fde89bedea2fb8ad497ebe134bff5
Isabel Eclipse <Tardes@GET-09>
2025-10-01 16:46:22 +0200

    clase  con los metodos estaticos

---
+++ src/es/cursojava/interfaces/producto/Operaciones.java
@@ -0,0 +1,36 @@
+package es.cursojava.interfaces.producto;
+
+public class Operaciones {
+	static void selectObject(Object obj) {
+        if (obj instanceof Consultable) {
+            ((Consultable) obj).select();
+        } else {
+            System.out.println("Este objeto no se puede consultar.");
+        }
+    }
+	static void insertObject(Object obj) {
+        if (obj instanceof Insertable) {
+            ((Insertable) obj).insert();
+        } else {
+            System.out.println("Este objeto no se puede insertar.");
+        }
+    }
+
+	static void updateObject(Object obj) {
+		 if (obj instanceof Updatable) {
+	            ((Updatable) obj).update();
+	        } else {
+	            System.out.println("Este objeto no se puede actualizar.");
+	        }
+	}
+	
+	static void deleteObject(Object obj) {
+		 if (obj instanceof Deletable) {
+	            ((Deletable) obj).delete();
+	        } else {
+	            System.out.println("Este objeto no se puede eliminar.");
+	        }
+	}
+
+
+}
